f1eb1ec23e9cf9ab7aaa6c389aad33bed0e32744
Sacdia22 <your_email@example.com>
2026-05-20 19:04:14 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 10, "M", "Brown", "Calm", 1),
+
+("Rowdy", 5, "M", "Black", "Aggressive", 1),
+
+("Tabitha", 7, "F", "White", "Friendly", 1),
+
+("Sergeant Brown", 12, "M", "Brown", "Strict", 1),
+
+("Melissa", 4, "F", "Golden", "Playful", 1),
+
+("Grinch", 9, "M", "Green", "Grumpy", 0),
+
+("Wendy", 6, "F", "Brown", "Sweet", 1),
+
+(NULL, 3, "F", "Black", "Shy", 1);
